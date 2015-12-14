12565ab18b73aad792536c7a8abf00c99089e415
VivyGuo <tuer208@sina.com>
2015-12-14 17:26:01 +0800

    .

---
+++ GWNews/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  GWNews
+//
+//  Created by guowei on 15/12/8.
+//  Copyright © 2015年 guowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
